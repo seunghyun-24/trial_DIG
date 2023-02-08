b15a752558f72206bf709077d3b6c7615b436c1b
seunghyun-24 <98291947+seunghyun-24@users.noreply.github.com>
2023-02-08 15:24:06 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,4 +1,1 @@
 from dig.threedgraph.dataset import QM93D
-from dig.threedgraph.method import SphereNet
-from dig.threedgraph.evaluation import ThreeDEvaluator
-from dig.threedgraph.method import run
