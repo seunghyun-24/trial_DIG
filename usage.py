43682df0d503f18863af6678bdabc80cba5c9024
seunghyun-24 <98291947+seunghyun-24@users.noreply.github.com>
2023-02-08 16:11:56 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,1 +1,6 @@
-from dig.threedgraph.dataset import QM93D
+import json
+from dig.ggraph.dataset import ZINC250k
+from torch_geometric.loader import DenseDataLoader
+conf = json.load(open('config/rand_gen_zinc250k_config_dict.json'))
+dataset = ZINC250k(one_shot=False, use_aug=True)
+loader = DenseDataLoader(dataset, batch_size=conf['batch_size'], shuffle=True)
