a59ecd2c85ad5c75f9cb3d7866ac570ec512a531
seunghyun-24 <98291947+seunghyun-24@users.noreply.github.com>
2023-02-08 16:36:33 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,6 +1,10 @@
-import json
-from dig.ggraph.dataset import ZINC250k
-from torch_geometric.loader import DenseDataLoader
-conf = json.load(open('config/rand_gen_zinc250k_config_dict.json'))
-dataset = ZINC250k(one_shot=False, use_aug=True)
-loader = DenseDataLoader(dataset, batch_size=conf['batch_size'], shuffle=True)
+import torch
+import os.path as osp
+from dig.xgraph.dataset import SynGraphDataset
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+dataset = SynGraphDataset('./datasets', 'BA_shapes')
+dataset.data.x = dataset.data.x.to(torch.float32)
+dataset.data.x = dataset.data.x[:, :1]
+dim_node = dataset.num_node_features
+dim_edge = dataset.num_edge_features
+num_classes = dataset.num_classes
