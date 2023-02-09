3e40e24a70ddee1949a3d5c4c8dfcec9448b8d1d
seunghyun-24 <98291947+seunghyun-24@users.noreply.github.com>
2023-02-09 15:45:54 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,8 +1,21 @@
 import torch
-from torch_geometric.data import Data
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
 
-edge_index = torch.tensor([[0, 1, 1, 2],
-                           [1, 0, 2, 1]], dtype=torch.long)
-x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
+import torchvision
+import torchvision.transforms as transforms
 
-data = Data(x=x, edge_index=edge_index)
+import os
+import argparse
+
+from utils import progress_bar
+
+from gnn_explain import gnn_explain
+
+
+
+explainer = gnn_explain(6, 30,  1, 50)  ####arguments: (max_node, max_step, target_class, max_iters)
+
+explainer.train()
