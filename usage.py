e3ed7003c1970e37762137964f52a6d2aaac9e33
seunghyun-24 <98291947+seunghyun-24@users.noreply.github.com>
2023-02-10 13:40:00 +0900

    Update usage.py

--- usage.py
+++ usage.py
@@ -7,7 +7,6 @@ import time
 
 ## Imports for plotting
 import matplotlib.pyplot as plt
-matplotlib inline
 from IPython.display import set_matplotlib_formats
 set_matplotlib_formats('svg', 'pdf') # For export
 from matplotlib.colors import to_rgb
@@ -16,3 +15,23 @@ matplotlib.rcParams['lines.linewidth'] = 2.0
 import seaborn as sns
 sns.reset_orig()
 sns.set()
+
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+# Torchvision
+import torchvision
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+# PyTorch Lightning
+try:
+    import pytorch_lightning as pl
+except ModuleNotFoundError: # Google Colab does not have PyTorch Lightning installed by default. Hence, we do it here if necessary
+    !pip install --quiet pytorch-lightning>=1.4
+    import pytorch_lightning as pl
+from pytorch_lightning.callbacks import Learn
